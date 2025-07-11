5c95c9ee50419b0e8c4e7da47e16060011d01037
Mikhail Rozhkov <104309041+Azaro68@users.noreply.github.com>
2025-07-11 11:50:17 +0300

    Update HeaderPage.vue

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -48,7 +48,7 @@ export default {
       try {
         const res = await fetch(import.meta.env.BASE_URL + 'data/details.xlsx');
         const arrayBuffer = await res.arrayBuffer();
-        const workbook = XLSX.read(arrayBuffer, { type: 'buffer' });
+        const workbook = XLSX.read(arrayBuffer, { type: 'array' });
 
         const sheet = workbook.Sheets[workbook.SheetNames[0]];
         const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
